f19d8416c6b179a04699d87edb0e6e57997a765a
Szymon Leyk <biuro@szymonleyk.pl>
2021-01-25 22:07:54 +0100

    #1 dzielenie tekstu na akapity

--- src/Examples.java
+++ src/Examples.java
@@ -104,5 +104,38 @@ public class Examples {
         newText = text.strip().replaceAll(",", "").replaceAll(" ",";").toLowerCase();
         System.out.println(newText);
 
+        System.out.println();
+
+        System.out.println("--------------- podziel na akapity ---------------");
+        String loremIpsumText = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. " +
+                "Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.";
+        System.out.println(loremIpsumText);
+
+        System.out.println();
+
+        // znajdź drugie wystąpienie ". "
+        String loremIpsumText1 = loremIpsumText.replaceAll("\\. ", "\\. \n\n");
+
+        // pierwszy akapit
+        System.out.println("----------------------------");
+
+
+        int beginIndex = 0;
+        int secondDot;
+        do {
+            int firstDot = loremIpsumText.indexOf(". ", beginIndex);
+            secondDot = loremIpsumText.indexOf(". ", firstDot + 1);
+
+            // wyciąnięcie podzbioru
+            if(secondDot == -1){
+                loremIpsumText1 = loremIpsumText.substring(beginIndex);
+            } else {
+                loremIpsumText1 = loremIpsumText.substring(beginIndex, secondDot + 2);
+            }
+            System.out.println(loremIpsumText1+"\n\n");
+            beginIndex = secondDot+2;
+        } while (secondDot != -1);
+
+
     }
 }
